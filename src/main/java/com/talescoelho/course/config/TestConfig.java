a9556938a7e7f84fa5dd982275929bc729808d47
tales coelho <talisbastosmma@gmail.com>
2025-02-15 21:08:02 -0300

    Payment, one-to-one association

--- src/main/java/com/talescoelho/course/config/TestConfig.java
+++ src/main/java/com/talescoelho/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.talescoelho.course.entities.Category;
 import com.talescoelho.course.entities.Order;
 import com.talescoelho.course.entities.OrderItem;
+import com.talescoelho.course.entities.Payment;
 import com.talescoelho.course.entities.Product;
 import com.talescoelho.course.entities.User;
 import com.talescoelho.course.entities.enums.OrderStatus;
@@ -35,7 +36,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
@@ -54,14 +55,14 @@ public class TestConfig implements CommandLineRunner {
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -73,13 +74,18 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
+
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
-		
+
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o2);
+
+		o2.setPayment(pay1);
+		orderRepository.save(o2);
 	}
 
 }
